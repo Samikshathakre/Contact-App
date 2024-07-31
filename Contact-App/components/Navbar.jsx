db5fd4276b6d3daf15b9f71f8f6eeddd5e7a63db
Samiksha Thakre <114510124+Samikshathakre@users.noreply.github.com>
2024-07-31 21:19:26 +0530

    Add files via upload

---
+++ Contact-App/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+        <img src="/logos_firebase.svg" />
+        <h1>Contact App</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
